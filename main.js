28ff5232a1dc208e0d0a0a7291d7a595f3ef5db5
akmathesh <akmathesh2023@gmail.com>
2024-06-01 16:28:48 +0530

    route structure

--- main.js
+++ main.js
@@ -8,6 +8,19 @@ app.get("/", (req, res) => {
   res.json({ msg: "Hello Students!!" });
 });
 
+// CRUD functionality of movies
+// R - Reading
+app.get("/movies", (req, res) => {});
+
+// C - Create
+app.post("/movies", (req, res) => {});
+
+// U - Update
+app.put("/movies/:id", (req, res) => {});
+
+// D - Delete
+app.delete("/movies/:id", (req, res) => {});
+
 app.listen(PORT, () => {
   console.log(`This server is running at http://localhost:${PORT}`);
 });
